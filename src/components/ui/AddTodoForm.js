744b75c3b23af871e4dd8d1264ba7b2d7867738f
Anthony Vest <anthony.vest@outlook.com>
2020-06-03 12:19:28 +0100

    updated to add todo form to use reducer hook

--- src/components/ui/AddTodoForm.js
+++ src/components/ui/AddTodoForm.js
@@ -1,26 +1,55 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 
+const initialState = {
+    todo: '',
+    isError: false,
+    isFormValid: false
+}
+const actions = {
+    TYPING : 'TYPING',
+    SUBMIT: 'SUBMIT',
+    RESET: 'RESET',
+    INVALID: 'INVALID'
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case actions.TYPING:
+           return {
+               ...state,
+               todo: action.value
+           }
+        case actions.SUBMIT:
+           const isError = state.todo.trim().length === 0;
+           return isError ? { ...state, isError: true } : {...state};
+        case actions.RESET:
+            return { ...state, isError: false, todo: '' };
+        case actions.INVALID:
+            return { ...state, isError: true, todo: '' };
+        default:
+            return state;
+    }
+}
 
 const AddTodoForm = ({ onSubmit }) => {
-    const [todo, setTodo] = useState('');
-    const [error, setError] = useState(false);
+    const [state, dispatch] = useReducer(reducer, initialState);
+    
     const submitHandler = (e) => {
         e.preventDefault();
-        if(todo.trim() === ''){
-            setError(true);
-            return;
+        dispatch({type: actions.SUBMIT });
+        if(state.todo.trim().length === 0){
+            return dispatch({type: actions.INVALID });
         }
-        onSubmit(todo);
-        setTodo('');
-        setError(false);
+        onSubmit(state.todo);
+        dispatch({type: actions.RESET });
     }
-    const err = error === true ? <span style={{color: 'red'}}>Todo can't be empty</span> : null
+    const err = state.isError === true ? <span style={{color: 'red'}}>Todo can't be empty</span> : null
     return (
         <div>
             <form onSubmit={submitHandler}>
-                <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)}/>
-                <button disabled={todo === ''} type="submit">Add Todo</button>    
+                <input type="text" value={state.todo} onChange={(e) => dispatch({type: actions.TYPING, value: e.target.value})}/>
+                <button disabled={state.todo === ''} type="submit">Add Todo</button>    
                 <br/>
                 {err}
             </form>
